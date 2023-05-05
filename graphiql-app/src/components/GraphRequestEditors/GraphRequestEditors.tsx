1a4daf94611e2ce997377cff225abc48a0185465
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-05-06 00:38:00 +0600

    feat: add rtk query and refactor graphql request

--- graphiql-app/src/components/GraphRequestEditors/GraphRequestEditors.tsx
+++ graphiql-app/src/components/GraphRequestEditors/GraphRequestEditors.tsx
@@ -1,37 +1,23 @@
-import { useState } from 'react';
-import { useAppDispatch, useAppSelector } from '../../redux/hook';
+import { useAppSelector } from '../../redux/hook';
 import styles from './GraphRequestEditor.module.scss';
 
 import RequestSection from './RequestSection/RequestSection';
 import VariablesSection from './ParamsSection/VariablesSection';
+import { useGetGraphqlMutation } from '../../redux/graphqlApi';
 
 const GraphRequestEditors = () => {
   const querySchema = useAppSelector((state) => state.requestSchema);
   const queryVariables = useAppSelector((state) => state.requestVariables);
 
-  const [responseData, setResponseData] = useState('');
-
-  const url = 'https://rickandmortyapi.com/graphql';
-
-  const makeRequest = async (query: string, variables: string) => {
-    console.log('QUERY', query);
-    console.log('VARIABLES', variables);
-    const res = await fetch(url, {
-      method: 'POST',
-      headers: { 'Content-type': 'application/json' },
-      body: JSON.stringify({
-        query: query,
-        variables: variables ? JSON.parse(variables) : {},
-      }),
-    });
-
-    const data = await res.json();
-    return data;
-  };
+  const [trigger, { data }] = useGetGraphqlMutation();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setResponseData(JSON.stringify(await makeRequest(querySchema, queryVariables), null, 4));
+    const body = JSON.stringify({
+      query: querySchema,
+      variables: queryVariables ? JSON.parse(queryVariables) : {},
+    });
+    await trigger(body);
   };
 
   return (
@@ -50,7 +36,7 @@ const GraphRequestEditors = () => {
         <button type="submit">Get</button>
       </form>
       <pre>
-        <code>{responseData}</code>
+        <code>{JSON.stringify(data, null, 4)}</code>
       </pre>
     </div>
   );
--- graphiql-app/src/redux/store/store.ts
+++ graphiql-app/src/redux/store/store.ts
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import requestSchemaReducer from './requestSchemaSlice';
 import requestVariablesReducer from './requestVariablesSlice';
+import { graphqlApi } from '../graphqlApi';
 
 const store = configureStore({
   reducer: {
     requestSchema: requestSchemaReducer,
     requestVariables: requestVariablesReducer,
+    [graphqlApi.reducerPath]: graphqlApi.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(graphqlApi.middleware),
 });
 
 export default store;
