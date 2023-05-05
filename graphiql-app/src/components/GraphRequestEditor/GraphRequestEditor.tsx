75b2cedd3d941f6714dfffa6f7b73780bc0cac41
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-05-05 19:55:13 +0600

    feat: add editor for variables section and refactor code

--- graphiql-app/src/components/GraphRequestEditor/GraphRequestEditor.tsx
+++ graphiql-app/src/components/GraphRequestEditor/GraphRequestEditor.tsx
@@ -1,66 +1,24 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../redux/hook';
-import { addRequestSchema } from '../../redux/store/requestSchemaSlice';
 import styles from './GraphRequestEditor.module.scss';
-import { addRequestVariables } from '../../redux/store/requestVariablesSlice';
-import { myTheme } from './editorStyles';
-import { myHighlightStyle } from './editorStyles';
 
-import { EditorView, keymap, lineNumbers } from '@codemirror/view';
-import { defaultKeymap, history } from '@codemirror/commands';
-import { autocompletion, closeBrackets } from '@codemirror/autocomplete';
-import { bracketMatching, syntaxHighlighting } from '@codemirror/language';
-import { graphql } from 'cm6-graphql';
-import { GraphQLSchema, buildClientSchema, getIntrospectionQuery } from 'graphql';
+import RequestSection from './RequestSection/RequestSection';
+import VariablesSection from './ParamsSection/VariablesSection';
 
 const GraphRequestEditor = () => {
-  const dispatch = useAppDispatch();
-  const addQuerySchema = (query: string) => dispatch(addRequestSchema(query));
-  const addVariables = (variables: string) => dispatch(addRequestVariables(variables));
   const querySchema = useAppSelector((state) => state.requestSchema);
   const queryVariables = useAppSelector((state) => state.requestVariables);
 
   const [responseData, setResponseData] = useState('');
-  const [docSchema, setDocSchema] = useState<GraphQLSchema>();
-  const [element, setElement] = useState<HTMLElement>();
 
   const url = 'https://rickandmortyapi.com/graphql';
-
-  useEffect(() => {
-    async function remoteSchema(url: string) {
-      const { data, errors } = await fetch(url, {
-        method: 'post',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          operationName: 'IntrospectionQuery',
-          query: getIntrospectionQuery(),
-        }),
-      }).then((res) => res.json());
-
-      if (errors) {
-        throw new Error('Error fetching remote schema!');
-      }
-      setDocSchema(buildClientSchema(data));
-      return buildClientSchema(data);
-    }
-    remoteSchema(url);
-    // console.log('get schema')
-  }, []);
-
-  const ref = useCallback((node: HTMLElement | null) => {
-    if (!node) return;
-
-    setElement(node);
-  }, []);
 
   const makeRequest = async (query: string, variables: string) => {
     console.log('QUERY', query);
     console.log('VARIABLES', variables);
     const res = await fetch(url, {
       method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-      },
+      headers: { 'Content-type': 'application/json' },
       body: JSON.stringify({
         query: query,
         variables: variables ? JSON.parse(variables) : {},
@@ -71,38 +29,6 @@ const GraphRequestEditor = () => {
     return data;
   };
 
-  useEffect(() => {
-    if (!element && !docSchema) return;
-
-    const view = new EditorView({
-      doc: querySchema,
-      extensions: [
-        EditorView.updateListener.of((e) => {
-          addQuerySchema(e.state.doc.toString());
-        }),
-        myTheme,
-        bracketMatching(),
-        closeBrackets(),
-        history(),
-        autocompletion(),
-        lineNumbers(),
-        keymap.of(defaultKeymap),
-        syntaxHighlighting(myHighlightStyle),
-        graphql(docSchema, {
-          onShowInDocs(field, type, parentType) {
-            alert(`Showing in docs.: Field: ${field}, Type: ${type}, ParentType: ${parentType}`);
-          },
-          onFillAllFields(view, schema, _query, cursor, token) {
-            alert(`Filling all fields. Token: ${token}`);
-          },
-        }),
-      ],
-      parent: element,
-    });
-
-    return () => view.destroy();
-  }, [element, docSchema]);
-
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setResponseData(JSON.stringify(await makeRequest(querySchema, queryVariables), null, 4));
@@ -111,8 +37,17 @@ const GraphRequestEditor = () => {
   return (
     <div className={styles.container}>
       <form onSubmit={handleSubmit}>
-        <div className={styles.request_editor} ref={ref}></div>
-        <textarea value={queryVariables} onChange={(e) => addVariables(e.target.value)} />
+        <RequestSection />
+        <div className={styles.params_buttons}>
+          <button type="button" className={styles.variables_button}>
+            Variables
+          </button>
+          <button type="button" className={styles.variables_button}>
+            Headers
+          </button>
+        </div>
+        <VariablesSection />
+        {/* <textarea value={queryVariables} onChange={(e) => addVariables(e.target.value)} /> */}
         <button type="submit">Get</button>
       </form>
       <pre>
---
+++ graphiql-app/src/components/GraphRequestEditor/RequestSection/RequestSection.tsx
@@ -0,0 +1,80 @@
+import { useEffect, useRef, useState } from 'react';
+
+import { EditorView, keymap, lineNumbers } from '@codemirror/view';
+import { defaultKeymap, history } from '@codemirror/commands';
+import { autocompletion, closeBrackets } from '@codemirror/autocomplete';
+import { bracketMatching, syntaxHighlighting } from '@codemirror/language';
+import { graphql } from 'cm6-graphql';
+import { myTheme } from '../editorStyles';
+import { myHighlightStyle } from '../editorStyles';
+import { useAppDispatch, useAppSelector } from '../../../redux/hook';
+import { addRequestSchema } from '../../../redux/store/requestSchemaSlice';
+import { GraphQLSchema, buildClientSchema, getIntrospectionQuery } from 'graphql';
+
+const RequestSection = () => {
+  const requestEditorParent = useRef(null);
+
+  const dispatch = useAppDispatch();
+  const addQuerySchema = (query: string) => dispatch(addRequestSchema(query));
+  const querySchema = useAppSelector((state) => state.requestSchema);
+
+  const [docSchema, setDocSchema] = useState<GraphQLSchema>();
+
+  const url = 'https://rickandmortyapi.com/graphql';
+
+  useEffect(() => {
+    async function remoteSchema(url: string) {
+      const { data, errors } = await fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          operationName: 'IntrospectionQuery',
+          query: getIntrospectionQuery(),
+        }),
+      }).then((res) => res.json());
+
+      if (errors) {
+        throw new Error('Error fetching remote schema!');
+      }
+      setDocSchema(buildClientSchema(data));
+      return buildClientSchema(data);
+    }
+    remoteSchema(url);
+  }, []);
+
+  useEffect(() => {
+    if (!requestEditorParent || !docSchema) return;
+
+    const view = new EditorView({
+      doc: querySchema,
+      extensions: [
+        EditorView.updateListener.of((e) => {
+          addQuerySchema(e.state.doc.toString());
+        }),
+        myTheme,
+        bracketMatching(),
+        closeBrackets(),
+        history(),
+        autocompletion(),
+        lineNumbers(),
+        keymap.of(defaultKeymap),
+        syntaxHighlighting(myHighlightStyle),
+        graphql(docSchema, {
+          onShowInDocs(field, type, parentType) {
+            alert(`Showing in docs.: Field: ${field}, Type: ${type}, ParentType: ${parentType}`);
+          },
+          onFillAllFields(view, schema, _query, cursor, token) {
+            alert(`Filling all fields. Token: ${token}`);
+          },
+        }),
+      ],
+      parent: requestEditorParent.current!,
+    });
+    console.log('Request editor')
+    return () => view.destroy();
+  }, [requestEditorParent.current, docSchema]);
+
+  return <div ref={requestEditorParent}></div>;
+};
+
+export default RequestSection;
