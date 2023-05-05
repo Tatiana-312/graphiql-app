9370ac1df9e0de0c7d345515f9734a57f987ebdb
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-05-05 20:29:26 +0600

    feat: add main page

---
+++ graphiql-app/src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,11 @@
+import GraphRequestEditors from '../../components/GraphRequestEditors/GraphRequestEditors';
+
+const MainPage = () => {
+  return (
+    <div>
+      <GraphRequestEditors />
+    </div>
+  );
+};
+
+export default MainPage;
