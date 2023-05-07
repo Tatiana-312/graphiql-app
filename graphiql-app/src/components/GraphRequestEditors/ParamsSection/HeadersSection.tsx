108f926596717ddc34f461359678f2ed90abef74
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-05-07 20:40:00 +0600

    feat: add headers state

--- graphiql-app/src/components/GraphRequestEditors/ParamsSection/HeadersSection.tsx
+++ graphiql-app/src/components/GraphRequestEditors/ParamsSection/HeadersSection.tsx
@@ -5,24 +5,24 @@ import { closeBrackets } from '@codemirror/autocomplete';
 import { bracketMatching, syntaxHighlighting } from '@codemirror/language';
 import { myHighlightStyle } from '../editorStyles';
 import { useAppDispatch, useAppSelector } from '../../../hooks/redux-hooks';
-import { addRequestVariables } from '../../../redux/store/requestVariablesSlice';
 import { myVariablesCloseTheme, myVariablesOpenTheme } from './paramsEditorStyle';
+import { addRequestHeaders } from '../../../redux/store/requestHeadersSlice';
 
 const HeadersSection = () => {
   const headersEditorParent = useRef(null);
   const dispatch = useAppDispatch();
-  const addVariables = (variables: string) => dispatch(addRequestVariables(variables));
-  const queryVariables = useAppSelector((state) => state.requestVariables);
+  const addHeaders = (headers: string) => dispatch(addRequestHeaders(headers));
+  const queryHeaders = useAppSelector((state) => state.requestHeaders);
   const isShown = useAppSelector((state) => state.displayVariablesSection.active);
 
   useEffect(() => {
     if (!headersEditorParent) return;
     const view = new EditorView({
-      doc: 'queryHeaders',
+      doc: queryHeaders,
       extensions: [
-        // EditorView.updateListener.of((e) => {
-        //   addVariables(e.state.doc.toString());
-        // }),
+        EditorView.updateListener.of((e) => {
+          addHeaders(e.state.doc.toString());
+        }),
         isShown ? myVariablesOpenTheme : myVariablesCloseTheme,
         bracketMatching(),
         closeBrackets(),
