9548fbc0578d8a4977e99c61cb66c413281e31ef
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-05-08 21:32:30 +0600

    refactor: refactor responseSection

---
+++ graphiql-app/src/components/GraphResponse/responseEditorStyles.ts
@@ -0,0 +1,31 @@
+import { EditorView } from '@codemirror/view';
+
+const generalTheme = {
+  '&': {
+    width: '100%',
+    color: '#ddd',
+    fontSize: '17px',
+    overflow: 'auto',
+  },
+  '.cm-content': {
+    caretColor: '#fc6',
+  },
+  '&.cm-focused .cm-selectionBackground, ::selection': {
+    backgroundColor: '#7272726a',
+  },
+  '&.cm-focused': {
+    outline: 'none',
+  },
+  '.cm-scroller': { overflow: 'auto' },
+};
+
+export const myResponseTheme = EditorView.theme(
+  {
+    generalTheme,
+    '.response > &': {
+      height: '100vh',
+      backgroundColor: '#494949',
+    },
+  },
+  { dark: true }
+);
