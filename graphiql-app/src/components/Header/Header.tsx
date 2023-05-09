2147a480e1a6eebae0cbfa5342dcebb741f50feb
DianaSmertina <am.dianasmertina@gmail.com>
2023-05-09 20:28:10 +0400

    feat: add app info and adaptive

--- graphiql-app/src/components/Header/Header.tsx
+++ graphiql-app/src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import styles from './Header.module.scss';
+import { BurgerMenu } from './BurgerMenu';
 
 export function Header(props: { isAuthorized: boolean }) {
   const [scrolled, setScrolled] = useState(false);
   const [language, setLanguage] = useState('ENG');
+  const [burger, setBurger] = useState(false);
   const { t, i18n } = useTranslation();
 
   useEffect(() => {
@@ -21,31 +23,39 @@ export function Header(props: { isAuthorized: boolean }) {
     return () => window.removeEventListener('scroll', onScroll);
   }, []);
 
-  const clickHandler = () => {
+  const clickLangHandler = () => {
     const toggledLanguage = language === 'ENG' ? 'RUS' : 'ENG';
     setLanguage(toggledLanguage);
     i18n.changeLanguage(toggledLanguage);
   };
 
+  const clickBurgerHandler = () => {
+    setBurger((prevState) => !prevState);
+  };
+
   return (
     <header className={scrolled ? styles.header + ' ' + styles.scrolled : styles.header}>
-      <div className={'wrapper ' + styles.headerWraper}>
-        <div className={styles.graphqlHeader}>
-          <div className={styles.logo}></div>
-          <h1>GraphQL</h1>
-        </div>
-        <div className={styles.userBtns}>
-          <div className={styles.language} onClick={clickHandler}>
-            {language}
+      {burger ? <BurgerMenu onClick={clickBurgerHandler} /> : null}
+      <div className={'wrapper '}>
+        <div className={styles.headerWraper}>
+          <div className={styles.graphqlHeader}>
+            <div className={styles.logo}></div>
+            <h1 className={styles.title}>GraphiQL</h1>
           </div>
-          {props.isAuthorized ? (
-            <button className={'btn ' + styles.signBtn}>{t('sign-out')}</button>
-          ) : (
-            <>
-              <button className={'btn ' + styles.signBtn}>{t('sign-in')}</button>
-              <button className={'btn ' + styles.signBtn}>{t('sign-up')}</button>
-            </>
-          )}
+          <div className={styles.userBtns}>
+            <div className={styles.language} onClick={clickLangHandler}>
+              {language}
+            </div>
+            {props.isAuthorized ? (
+              <button className={'btn ' + styles.signBtn}>{t('sign-out')}</button>
+            ) : (
+              <>
+                <div className={styles.burger} onClick={clickBurgerHandler}></div>
+                <button className={'btn ' + styles.signBtn}>{t('sign-in')}</button>
+                <button className={'btn ' + styles.signBtn}>{t('sign-up')}</button>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </header>
--- graphiql-app/src/pages/WelcomePage/WelcomePage.tsx
+++ graphiql-app/src/pages/WelcomePage/WelcomePage.tsx
@@ -5,19 +5,39 @@ import styles from './WelcomePage.module.scss';
 
 export function WelcomePage() {
   const { t } = useTranslation();
-  const isAuthorized = true;
+  const isAuthorized = false;
+
+  function createList() {
+    const arr = new Array(5).fill(0).map((el, i) => {
+      const iconsClass = 'featureIcon' + i;
+      return (
+        <li className={styles.feature} key={i}>
+          <span className={styles[iconsClass] + ' ' + styles.featureIcon}></span>
+          {t('feature' + i)}
+        </li>
+      );
+    });
+    return arr;
+  }
 
   return (
     <>
       <Header isAuthorized={isAuthorized} />
       <main className="main">
         <div className={styles.welcome}>
-          <h2 className={styles.greeting}>{t('welcome')}</h2>
-          {isAuthorized && <button className={'btn ' + styles.getStartedBtn}>{t('start')}</button>}
+          <div className="wrapper">
+            <h2 className={styles.greeting}>{t('welcome')}</h2>
+            {isAuthorized && (
+              <button className={'btn ' + styles.getStartedBtn}>{t('start')}</button>
+            )}
+          </div>
         </div>
-        <div className={styles.aboutUs}>
-          <h3>{t('descrition-header')}</h3>
-          <p className={styles.description}>{t('description')}</p>
+        <div className="wrapper">
+          <div className={styles.aboutApp}>
+            <h3 className={styles.descriptionHeader}>{t('descrition-header')}</h3>
+            <p className={styles.description}>{t('description')}</p>
+            <ul>{createList()}</ul>
+          </div>
         </div>
       </main>
     </>
