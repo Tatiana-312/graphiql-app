64c8a01eb5e8bc797f6e6924bff4f34358664b11
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-05-10 19:29:51 +0600

    refactor: add OptionsEditor instead of Variables and Headers

--- graphiql-app/src/components/GraphRequestEditors/OptionsSection/OptionsEditor.tsx
+++ graphiql-app/src/components/GraphRequestEditors/OptionsSection/OptionsEditor.tsx
@@ -1,28 +1,30 @@
-import { FC, useEffect, useRef } from 'react';
+import { FC, useEffect } from 'react';
 import { EditorView, keymap, lineNumbers } from '@codemirror/view';
 import { defaultKeymap } from '@codemirror/commands';
 import { closeBrackets } from '@codemirror/autocomplete';
 import { bracketMatching, syntaxHighlighting } from '@codemirror/language';
 import { myHighlightStyle } from '../editorStyles';
-import { useAppDispatch, useAppSelector } from '../../../hooks/redux-hooks';
+import { useAppSelector } from '../../../hooks/redux-hooks';
 import { myOptionsCloseTheme, myOptionsOpenTheme } from './optionsEditorStyle';
-import { addRequestHeaders } from '../../../redux/store/requestHeadersSlice';
 
-const HeadersSection: FC = () => {
-  const headersEditorParent = useRef(null);
-  const dispatch = useAppDispatch();
-  const addHeaders = (headers: string) => dispatch(addRequestHeaders(headers));
-  const queryHeaders = useAppSelector((state) => state.requestHeaders);
+interface OptionsProps {
+  name: string;
+  value: string;
+  parent: React.MutableRefObject<null>;
+  changeValue: (value: string) => void;
+}
+
+const OptionsEditor: FC<OptionsProps> = ({ name, value, parent, changeValue }) => {
   const isShown = useAppSelector((state) => state.displayVariablesSection.active);
 
   useEffect(() => {
-    if (!headersEditorParent) return;
+    if (!parent) return;
 
     const view = new EditorView({
-      doc: queryHeaders,
+      doc: value,
       extensions: [
         EditorView.updateListener.of((e) => {
-          addHeaders(e.state.doc.toString());
+          changeValue(e.state.doc.toString());
         }),
         isShown ? myOptionsOpenTheme : myOptionsCloseTheme,
         bracketMatching(),
@@ -31,13 +33,13 @@ const HeadersSection: FC = () => {
         keymap.of(defaultKeymap),
         syntaxHighlighting(myHighlightStyle),
       ],
-      parent: headersEditorParent.current!,
+      parent: parent.current!,
     });
 
     return () => view.destroy();
-  }, [isShown]);
+  }, [isShown, name]);
 
-  return <div className="headers-editor" ref={headersEditorParent}></div>;
+  return <div className={`${name}-editor`} ref={parent}></div>;
 };
 
-export default HeadersSection;
+export default OptionsEditor;
--- graphiql-app/src/components/GraphRequestEditors/OptionsSection/OptionsSection.tsx
+++ graphiql-app/src/components/GraphRequestEditors/OptionsSection/OptionsSection.tsx
@@ -2,21 +2,30 @@ import { useAppDispatch, useAppSelector } from '../../../hooks/redux-hooks';
 import Button from './Button';
 import styles from './OptionsSection.module.scss';
 import { changeDisplayVariablesSection } from '../../../redux/store/displayVariablesSectionSlice';
-import VariablesSection from './VariablesSection';
-import HeadersSection from './HeadersSection';
 import {
   openHeadersSection,
   openVariablesSection,
 } from '../../../redux/store/optionsSectionTypeSlice';
-import { FC } from 'react';
+import { FC, useRef } from 'react';
+import OptionsEditor from './OptionsEditor';
+import { addRequestVariables } from '../../../redux/store/requestVariablesSlice';
+import { addRequestHeaders } from '../../../redux/store/requestHeadersSlice';
 
 const OptionsSection: FC = () => {
+  const variablesEditorParent = useRef(null);
+  const headersEditorParent = useRef(null);
+
   const dispatch = useAppDispatch();
   const changeDisplay = () => dispatch(changeDisplayVariablesSection());
   const openVariables = () => dispatch(openVariablesSection());
   const openHeaders = () => dispatch(openHeadersSection());
   const sectionType = useAppSelector((state) => state.optionsSectionType);
   const isShown = useAppSelector((state) => state.displayVariablesSection.active);
+
+  const changeVariables = (variables: string) => dispatch(addRequestVariables(variables));
+  const queryVariables = useAppSelector((state) => state.requestVariables);
+  const changeHeaders = (headers: string) => dispatch(addRequestHeaders(headers));
+  const queryHeaders = useAppSelector((state) => state.requestHeaders);
 
   const [isVariablesActive, isHeadersActive] =
     sectionType === 'variables' ? [true, false] : [false, true];
@@ -30,7 +39,21 @@ const OptionsSection: FC = () => {
         </button>
         <Button name={'Headers'} onClick={() => openHeaders()} isActive={isHeadersActive} />
       </div>
-      {sectionType === 'variables' ? <VariablesSection /> : <HeadersSection />}
+      {sectionType === 'variables' ? (
+        <OptionsEditor
+          name="variables"
+          value={queryVariables}
+          parent={variablesEditorParent}
+          changeValue={changeVariables}
+        />
+      ) : (
+        <OptionsEditor
+          name="headers"
+          value={queryHeaders}
+          parent={headersEditorParent}
+          changeValue={changeHeaders}
+        />
+      )}
     </div>
   );
 };
