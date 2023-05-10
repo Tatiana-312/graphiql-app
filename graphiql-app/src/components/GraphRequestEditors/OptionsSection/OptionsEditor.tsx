fcba4e5e80fcff8da6fc270651dc1ba89e9bb889
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-05-10 21:19:33 +0600

    feat: add history() and json language for options editor

--- graphiql-app/src/components/GraphRequestEditors/OptionsSection/OptionsEditor.tsx
+++ graphiql-app/src/components/GraphRequestEditors/OptionsSection/OptionsEditor.tsx
@@ -1,8 +1,9 @@
 import { FC, useEffect } from 'react';
 import { EditorView, keymap, lineNumbers } from '@codemirror/view';
-import { defaultKeymap } from '@codemirror/commands';
+import { defaultKeymap, history } from '@codemirror/commands';
 import { closeBrackets } from '@codemirror/autocomplete';
 import { bracketMatching, syntaxHighlighting } from '@codemirror/language';
+import { json } from '@codemirror/lang-json';
 import { myHighlightStyle } from '../editorStyles';
 import { useAppSelector } from '../../../hooks/redux-hooks';
 import { myOptionsCloseTheme, myOptionsOpenTheme } from './optionsEditorStyle';
@@ -29,9 +30,11 @@ const OptionsEditor: FC<OptionsProps> = ({ name, value, parent, changeValue }) =
         isShown ? myOptionsOpenTheme : myOptionsCloseTheme,
         bracketMatching(),
         closeBrackets(),
+        history(),
         lineNumbers(),
         keymap.of(defaultKeymap),
         syntaxHighlighting(myHighlightStyle),
+        json(),
       ],
       parent: parent.current!,
     });
