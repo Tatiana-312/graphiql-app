178fd5d9bfb8e592acbd22ff0edd31d7776838c4
Altynbek312 <altyn_312@mail.ru>
2023-05-10 14:14:35 +0300

    refactor: delete extra files

--- graphiql-app/src/main.tsx
+++ graphiql-app/src/main.tsx
@@ -6,16 +6,13 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import '../firebase';
-import { AuthProvider } from './components/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* <AuthProvider> */}
       <Provider store={store}>
         <App />
       </Provider>
-      {/* </AuthProvider> */}
     </BrowserRouter>
   </React.StrictMode>
 );
